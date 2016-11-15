599eef08a0eb72eabab81ee4ad8f4d5d49884177
liran <koritze@gmail.com>
2016-11-15 09:58:40 +0200

    Add json and firebase connection

--- app/src/main/java/com/example/wfrelic/TypeDetailsActivity.java
+++ app/src/main/java/com/example/wfrelic/TypeDetailsActivity.java
@@ -1,9 +1,10 @@
 package com.example.wfrelic;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TypeDetailsActivity extends AppCompatActivity {
+public class TypeDetailsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
